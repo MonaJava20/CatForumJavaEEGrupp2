fe75f18833c4d7cb9376684159532dc4caa7cdf1
Johanna <johannatovebergstrom@gmail.com>
2021-10-20 15:34:42 +0200

    created projekt in spring initializr Hannes, Miranda, Monda, Johanna

---
+++ src/main/java/com/grupp2javaee/catforum/CatforumApplication.java
@@ -0,0 +1,13 @@
+package com.grupp2javaee.catforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatforumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatforumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupp2javaee/catforum/CatforumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupp2javaee.catforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatforumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
