836d56084f5e3e14d693e87b4f9eee97a978978d
HA-ANgit <hannes93andersson@gmail.com>
2021-10-27 12:44:04 +0200

    Fungerande kod, nickName Refaktorerad till username. Configure-metod som öppnar upp WebSecurityConfig

--- src/main/java/com/grupp2javaee/catforum/controller/AccountController.java
+++ src/main/java/com/grupp2javaee/catforum/controller/AccountController.java
@@ -3,9 +3,11 @@ package com.grupp2javaee.catforum.controller;
 import com.grupp2javaee.catforum.model.Account;
 import com.grupp2javaee.catforum.model.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.aggregation.ArithmeticOperators;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,11 +19,22 @@ public class AccountController {
     AccountRepository accountRepo;
 
     //Metoden login returnerar ett objekt av ModelAndView
-    /*public ModelAndView login() {
+    public ModelAndView login() {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("login");
         return modelAndView;
+    }
+
+    /*
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+        public Account<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));SecurityContextHolder.getContext().setAuthentication(authentication);String jwt = jwtUtils.generateJwtToken(authentication);UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();List<String> roles = userDetails.getAuthorities().stream().map(item -> item.getAuthority()).collect(Collectors.toList());return ResponseEntity.ok(new JwtResponse(jwt,
+            userDetails.getId(),
+            userDetails.getUsername(),
+            userDetails.getEmail(),
+            roles));
     }*/
+
+
 
     //GET create-formuläret.
     @RequestMapping(value = "/create", method = RequestMethod.GET)
@@ -53,6 +66,9 @@ public class AccountController {
         }
         return modelAndView;
     }
+
+    //UPDATE
+
 
     //@RequestMapping(value="/account", method = RequestMethod.GET)
 
